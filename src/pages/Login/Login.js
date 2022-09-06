d041ab5526c141bba5d62138cc4b113eba11d3bf
fil <filipisee@hotmail.com>
2022-09-06 10:42:33 -0300

    structuring css files

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,7 @@
+function Login() {
+  return (
+    <div>Home Page</div>
+  );
+}
+    
+export default Login;
---
+++ src/pages/Register/Register.js
@@ -0,0 +1,7 @@
+function Register() {
+  return (
+    <div>Register Page</div>
+  );
+}
+  
+export default Register;
