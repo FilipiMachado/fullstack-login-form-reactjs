4da2571b166015c2079410efe14b09ba3e432a7e
fil <filipisee@hotmail.com>
2022-09-06 18:42:54 -0300

    finished home page structure

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,16 +1,20 @@
 import { Link } from 'react-router-dom'
+import "./Home.css"
 
 function Home() {
   return (
     <div>
-      <div>
+      <div className="main">
+        <h1>Home Page</h1>
+      </div>
+      {/* <div>
         <Link to="/login">
           <button type="text">Go To Login Page</button>
         </Link>
         <Link to="register">
           <button type="text">Go To Register Page</button>
         </Link>
-      </div>
+      </div> */}
     </div>
   );
 }
